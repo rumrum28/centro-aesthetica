61c75960ca70308088b98709cd5a5159b222db18
cmd <crspyfry@gmail.com>
2023-06-23 18:04:17 +0800

    single product added

---
+++ src/components/services/services.tsx
@@ -0,0 +1,22 @@
+const SingleService = ({ product }) => {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{product.title}</div>
+        <p className="text-gray-700 text-base">Price: ${product.price}</p>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+        {product.tags.map((tag, index) => (
+          <span
+            key={index}
+            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >
+            #{tag}
+          </span>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default SingleService
